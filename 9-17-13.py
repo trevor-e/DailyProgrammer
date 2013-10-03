d501b21096213cf69937b591d85597437fc4acfb
Trevor Elkins <elkins.trevor@gmail.com>
2013-10-02 21:07:55 -0400

    accidentally committed old file

--- 9-17-13.py
+++ 9-17-13.py
@@ -1,6 +1,9 @@
 import sys
 from math import sqrt
 
+'''Calculates the repulsion force between two particles.  Was going to refactor this to create a particle class, but too lazy. :)
+Right now I perform no validation on the data being passed in either, though that would be pretty easy to add.'''
+
 def calculateDistance(p1x, p2x, p1y, p2y):
 	deltaX = p1x - p2x
 	deltaY = p1y - p2y
